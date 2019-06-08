adac8acd4b65770aee3899b4fc9e425be92c23dd
Hakim Cassimally <hakim.cassimally@gmail.com>
2019-06-08 16:03:23 +0100

    refactor to lanes

--- explain.py
+++ explain.py
@@ -27,133 +27,75 @@ make it easier to read.
 
 import itertools
 import math
-from itertools import (accumulate, chain, repeat, cycle)
+from itertools import (accumulate, chain, repeat, cycle, groupby, zip_longest)
 from functools import reduce
 from textwrap import wrap
 from operator import itemgetter
+import string
 
-def tokens(descs):
+def ntokens(descs):
     def norm(desc):
-        return desc if type(desc) is tuple else (desc, None)
+        (t,c) = desc if type(desc) is tuple else (desc, None)
+        return (t, c, len(t))
 
-    ts = [{'token': t,
-           'comment': c,
-           'length': len(t)}
-          for (t,c)
-          in [norm(desc) for desc in descs]]
-    return ts
+    raw = [norm(desc) for desc in descs]
+    header = ''.join([item[0] for item in raw])
 
-def decorate(ts, usePens=True):
     def offset(a, b):
-        o = a['offset'] + a['length']
-        po = o + math.ceil(b['length'] / 2)
-        return {**b,
-                'offset': o,
-                'pathOffset': po}
+        return (*b, a[3] + a[2])
 
-    init = {'offset': 0,
-            'pathOffset': 0,
-            'length': 0,
-            'comment': None}
+    ri = [(*raw[0], 0)] + raw[1:]
+    withOffsets = list(accumulate(ri, offset))
 
-    withOffset = list(filter(itemgetter('comment'),
-                             accumulate([init] + ts, offset)))
+    comment = itemgetter(1)
 
-    withDelta = [{**t,
-                  'indent': t['offset'] - p['offset'] - p['length'],
-                  'pathDelta': t['pathOffset'] - p['pathOffset']}
-                 for (t,p)
-                 in zip(withOffset, [init] + withOffset)]
-
-    pens = cycle([{'pen': False},
-                  {'pen': True}]) if usePens else repeat({'pen': False})
-    withPens = [{**t, **p}
-                for (t,p)
-                in zip(withDelta, pens)]
-    return withPens
-
+    return (header, list(filter(comment, withOffsets)))
 
 boxes = {
-        'ew': '─',
-        'EW': '━',
-        'ns': '│',
-        'NS': '┃',
-        'se': '┌',
-        'sE': '┍',
-        'Se': '┎',
-        'SE': '┏',
-        'sw': '┐',
-        'sW': '┑',
-        'Sw': '┒',
-        'SW': '┓',
-        'ne': '└',
-        'nE': '┕',
-        'Ne': '┖',
-        'NE': '┗',
-        'nw': '┘',
-        'nW': '┙',
-        'Nw': '┚',
-        'NW': '┛',
-        'nse': '├',
-        'nsE': '┝',
-        'Nse': '┞',
-        'nSe': '┟',
-        'NSe': '┠',
-        'NsE': '┡',
-        'nSE': '┢',
-        'NSE': '┣',
-        'nsw': '┤',
-        'nsW': '┥',
-        'Nsw': '┦',
-        'nSw': '┧',
-        'NSw': '┨',
-        'NsW': '┩',
-        'nSW': '┪',
-        'NSW': '┫',
-        'sew': '┬',
-        'seW': '┭',
-        'sEw': '┮',
-        'sEW': '┯',
-        'Sew': '┰',
-        'SeW': '┱',
-        'SEw': '┲',
-        'SEW': '┳',
-        'new': '┴',
-        'neW': '┵',
-        'nEw': '┶',
-        'nEW': '┷',
-        'New': '┸',
-        'NeW': '┹',
-        'NEw': '┺',
-        'NEW': '┻',
-        'nsew': '┼',
-        'nseW': '┽',
-        'nsEw': '┾',
-        'nsEW': '┿',
-        'Nsew': '╀',
-        'nSew': '╁',
-        'NSew': '╂',
-        'NseW': '╃',
-        'NsEw': '╄',
-        'nSeW': '╅',
-        'nSEw': '╆',
-        'NsEW': '╇',
-        'nSEW': '╈',
-        'NSeW': '╉',
-        'NSEw': '╊',
-        'NSEW': '╋',
-        'w': '╴',
-        'n': '╵',
-        'e': '╶',
-        's': '╷',
-        'W': '╸',
-        'N': '╹',
-        'E': '╺',
-        'S': '╻',
-        'Ew': '╼',
-        'nS': '╽',
-        'eW': '╾',
-        'Ns': '╿'}
+        'ew': '─', 'EW': '━', 'ns': '│', 'NS': '┃', 'se': '┌',
+        'sE': '┍', 'Se': '┎', 'SE': '┏', 'sw': '┐', 'sW': '┑',
+        'Sw': '┒', 'SW': '┓', 'ne': '└', 'nE': '┕', 'Ne': '┖',
+        'NE': '┗', 'nw': '┘', 'nW': '┙', 'Nw': '┚', 'NW': '┛',
+        'nse': '├', 'nsE': '┝', 'Nse': '┞', 'nSe': '┟', 'NSe': '┠',
+        'NsE': '┡', 'nSE': '┢', 'NSE': '┣', 'nsw': '┤', 'nsW': '┥',
+        'Nsw': '┦', 'nSw': '┧', 'NSw': '┨', 'NsW': '┩', 'nSW': '┪',
+        'NSW': '┫', 'sew': '┬', 'seW': '┭', 'sEw': '┮', 'sEW': '┯',
+        'Sew': '┰', 'SeW': '┱', 'SEw': '┲', 'SEW': '┳', 'new': '┴',
+        'neW': '┵', 'nEw': '┶', 'nEW': '┷', 'New': '┸', 'NeW': '┹',
+        'NEw': '┺', 'NEW': '┻', 'nsew': '┼', 'nseW': '┽', 'nsEw': '┾',
+        'nsEW': '┿', 'Nsew': '╀', 'nSew': '╁', 'NSew': '╂', 'NseW': '╃',
+        'NsEw': '╄', 'nSeW': '╅', 'nSEw': '╆', 'NsEW': '╇', 'nSEW': '╈',
+        'NSeW': '╉', 'NSEw': '╊', 'NSEW': '╋', 'w': '╴', 'n': '╵',
+        'e': '╶', 's': '╷', 'W': '╸', 'N': '╹', 'E': '╺',
+        'S': '╻', 'Ew': '╼', 'nS': '╽', 'eW': '╾', 'Ns': '╿'}
+
+box2recipe = {v: k for (k,v) in boxes.items() }
+
+def overlayc(a,b):
+    # anything beats a space
+    if a == ' ':
+        return b
+    elif b == ' ':
+        return a
+
+    # anything that isn't a box character wins
+    ar = box2recipe.get(a, None)
+    br = box2recipe.get(b, None)
+    if not ar:
+        return a
+    elif not br:
+        return b
+
+    # otherwise we have two box recipes! Compose them
+    return box(ar, br)
+
+def overlay2(aa,bb):
+    return ''.join([overlayc(a, b)
+                    for (a,b)
+                    in zip_longest(aa,bb, fillvalue=' ')])
+
+def overlay(items):
+    return reduce(overlay2, items, '')
 
 def box(*items):
     def norm(item):
@@ -175,96 +117,13 @@ def box(*items):
                    for d in 'nsew'])
     return boxes.get(dir, ' ')
 
-def boxer(pen):
-    return lambda item: box((item, pen))
-
-def header(ts):
-    print(''.join([t['token'] for t in ts]))
-
-def markers(ts):
-    def indent(t):
-        return ' ' * t['indent']
-
-    def line(t):
-        _ = boxer(t['pen'])
-        l = t['length']
-        if l == 1:
-            return _('ns')
-        elif l == 2:
-            return _('nse') + _('nw')
-        else:
-            left  = math.floor((l-3) / 2)
-            right = math.ceil((l-3) / 2)
-            return ''.join([
-                _('ne'),
-                left * _('ew'),
-                _('sew'),
-                right * _('ew'),
-                _('nw')])
-    def draw(t):
-        return indent(t) + line(t)
-
-    print(''.join([draw(t) for t in ts]))
-
-def converge(ts):
-    def aux(left, right):
-        (r,*rs) = right
-        span = r['pathOffset'] - len(left)
-
-        heads = [box(('ns', l['pen']))
-                 for l in left]
-
-        _ = boxer(r['pen'])
-        curve = _('ns') if span == 1 else _('se') + (_('ew') * (span-2)) + _('nw')
-
-        tails = [' ' * (r['pathDelta']-1)
-                 + box(('ns', r['pen']))
-                 for r in rs]
-
-        return ''.join([*heads,
-                       curve,
-                       *tails])
-
-    for x in range(0,len(ts)):
-        print(aux(ts[:x], ts[x:]))
+def flatten(lol):
+    return sum(lol, [])
 
 def reflow(text, width):
-    return sum([wrap(para, width)
-                for para
-                in text.split("\n")],
-               [])
-
-def definitions(ts, width):
-    l = len(ts)
-    for x in range(0,len(ts)):
-        current = ts[x]
-        pen = current['pen']
-        rest = ts[x+1:]
-        first = ''.join([' ' * x,
-                         box(('ne', pen)),
-                         *([box(('ew', pen), ('ns', t['pen']))
-                            for t in rest]),
-                         box(('ew', pen)),
-                         ' '])
-        then = ''.join([' ' * (x+1),
-                        *([box(('ns', t['pen']))
-                           for t in rest]),
-                        '  '])
-        text = reflow(ts[x]['comment'], width - (l+2))
-        print("\n".join(
-            [h+t
-             for (h,t)
-             in zip(chain([first], repeat(then)),
-                    text)]))
-
-def explain(descs, width=80, usePens=True):
-    ts = tokens(descs)
-    header(ts)
-
-    ts = decorate(ts, usePens=usePens)
-    markers(ts)
-    converge(ts)
-    definitions(ts, width)
+    return flatten([wrap(para, width)
+                    for para
+                    in text.split("\n")])
 
 
 descs = ['git',
@@ -278,9 +137,152 @@ descs = ['git',
          ('--name-status',
          """Show only names and status of changed files
 
-         for example:
-            M   foo.py"""),
+for example:
+   M   foo.py"""),
          ' ',
          ('<commit>', 'show differences between this commit and preceding one')]
 
-explain(descs, usePens=True)
+def drawat(pos, string):
+    return (' ' * pos) + string
+
+def drawhorizontal(a,b,pen):
+    (a,b) = sorted((a,b))
+    l = b-a
+    if l:
+        line = ''.join([box(('e',pen)),
+                        box(('ew',pen)) * (l-1),
+                        box(('w',pen))])
+        return drawat(a, line)
+    else:
+        return ''
+
+def drawverticals(point, items):
+    return overlay([drawat(pos, box((point, pen))) for (pos,pen,*_) in items])
+
+def resolve(existing, instructions, width=80):
+
+    fst = itemgetter(0)
+    snd = itemgetter(1)
+
+    n = drawverticals('n', existing)
+
+    exp = {pos: pen for (pos,pen,*_) in existing}
+    ext = {pos: text for (pos,_,text,*_) in existing}
+
+    def dest(i):
+        return list(filter(lambda a: type(a) is int, i))[-1]
+    def pen(i):
+        return next(filter(lambda a: type(a) is bool, i), False)
+    def text(i):
+        return next(filter(lambda a: type(a) is str, i), None)
+    def go(i):
+        return next(filter(lambda a: a is Ellipsis, i), None)
+
+    def normalise(instruction):
+        f = fst(instruction)
+        t = dest(instruction)
+        p = exp.get(f, pen(instruction))
+        tx = ext.get(f, text(instruction))
+        g = go(instruction)
+        return (f,t,p,tx,g)
+
+    normalised = [normalise(i) for i in instructions]
+
+    ew = overlay([drawhorizontal(a,b,pen)
+                  for (a,b,pen,*_)
+                  in normalised
+                  if a is not None and b is not None])
+
+    explicit = {fst(i) for i in normalised}
+    implicit = [(pos, *rest)
+                for (pos, *rest)
+                in existing
+                if not pos in explicit]
+
+    continuation = [(t,pen,comment)
+                    for (f,t,pen,comment,go) in normalised
+                    if not go]\
+                   + implicit
+
+    # sorted and merged paths
+    def aux(k,v):
+        v = list(v)
+        pen = any(map(snd, v))
+        [(_,_,comment), *_] = v
+        return (k, pen, comment)
+
+    nextState = [aux(k,v)
+                 for (k, v)
+                 in groupby(sorted(continuation, key=fst), fst)]
+
+    s = drawverticals('s', nextState)
+    lines = overlay([n, ew, s])
+
+    gone = next(filter(go, normalised), None)
+    if gone:
+        l = len(lines)
+        text = reflow(gone[3], width - l - 1)
+        (then, _) = resolve(nextState, [])
+        lines = ("\n".join(
+                    [h+t
+                     for (h,t)
+                     in zip(chain([lines + ' '], repeat(then + '   ')),
+                            text)]))
+
+    return (lines, nextState)
+
+def nmarkers(ts):
+    def aux(t):
+        (_,c,l,o) = t
+        end = o + l - 1
+        mean = o + int(l/2)
+        return (o, mean, end, c, None)
+
+    ps = list(map(aux, ts))
+    pens = cycle([False, True])
+    pre = flatten([[(start, pen, comment, None), (end, pen, comment, None)]
+                    for ((start, _, end, comment, _), pen)
+                    in zip(ps, pens)])
+
+    instructions = flatten([[(start, mean, comment, None), (end, mean, comment, None)]
+                            for (start, mean, end, comment, _)
+                            in ps])
+
+    return resolve(pre, instructions)
+
+def toleft_instructions(lanes):
+    def aux(lanes, idx):
+        pos = lanes[idx][0]
+        return (pos, idx)
+
+    return [[aux(lanes, idx)] for idx in range(0, len(lanes))]
+
+def define_instructions(lanes):
+    l = len(lanes) + 1
+    def aux(lane):
+        pos = lane[0]
+        return (pos, l, ...)
+
+    return [[aux(lane)] for lane in lanes]
+
+def resolve_list(lanes, instructions):
+    def aux(output_lanes, i):
+        (o, lanes) = output_lanes
+        (o2, lanes2) = resolve(lanes, i)
+        return (o + [o2], lanes2)
+
+    (output, lanes) = reduce(aux, instructions, ([], lanes))
+    return ('\n'.join(output), lanes)
+
+def explain(descs):
+    (header, ts) = ntokens(descs)
+    print(header)
+    (markers, lanes) = nmarkers(ts)
+    print(markers)
+    (left, lanes) = resolve_list(lanes, toleft_instructions(lanes))
+    print(left)
+    (define, lanes) = resolve_list(lanes, define_instructions(lanes))
+    print(define)
+
+
+explain(descs)
